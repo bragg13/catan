e55f5f745f19bcce518024e076fb0d89f8893a3d
Andrea <bragg003@gmail.com>
2023-06-21 21:49:12 +0200

    improved main menu with icons

--- client/src/GUI/MainMenu.jsx
+++ client/src/GUI/MainMenu.jsx
@@ -11,89 +11,136 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
+  Typography,
+  SvgIcon,
+  Stack,
+  Badge,
 } from "@mui/material";
 import BackpackIcon from "@mui/icons-material/Backpack";
 import ConstructionIcon from "@mui/icons-material/Construction";
 import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
-import './style.css'
+import "./style.css";
+import {inventory, crafting} from "../helpers/GUI_helpers"
 
 export default function MainMenu() {
   const [activeMenu, setActiveMenu] = useState(null);
 
-  const renderMenuItems = (items) => {
-    return <></>
-    // items.map((item, index) => (
-    //   <MenuItem key={index} onClick={handleClose}>
-    //     <ListItemIcon>{item.icon}</ListItemIcon>
-    //     <ListItemText primary={item.text} />
-    //   </MenuItem>
-    // ));
+  const renderCraftingItems = (items) => {
+    return crafting.map((res) => (
+      <Stack
+        sx={{ padding: "1vh" }}
+        gap={3}
+        direction={"row"}
+      >
+        <SvgIcon
+          sx={{ fontSize: "5vmin" }}
+          component={res.icon}
+          inheritViewBox
+        />
+        <Typography alignSelf={"center"}>
+          {items[res.name]}x {res.name}
+        </Typography>
+      </Stack>
+    ));
+  };
+
+  const renderInventoryItems = (items) => {
+    return inventory.map((res) => (
+      <Stack
+        sx={{ padding: "1vh" }}
+        gap={3}
+        direction={"row"}
+      >
+        <SvgIcon
+          sx={{ fontSize: "5vmin" }}
+          component={res.icon}
+          inheritViewBox
+        />
+        <Typography alignSelf={"center"}>
+          {items[res.name]}x {res.name}
+        </Typography>
+      </Stack>
+    ));
   };
 
   const menus = [
     {
       id: "build_menu",
       title: "Crafting",
-      buttonIcon: <ConstructionIcon sx={{ 
-        fontSize: '4vmin',
-        // transition: (activeMenu!==null && activeMenu.id === 'build_menu')
-      }} />,
+      buttonIcon: (
+        <ConstructionIcon
+          sx={{
+            fontSize: "4vmin",
+          }}
+        />
+      ),
       color: "success",
       disabled: false,
-      items: [
-        { icon: <ConstructionIcon />, text: "Item 1" },
-        { icon: <ConstructionIcon />, text: "Item 2" },
-      ],
+      items: {
+        town: 0,
+        city: 0,
+        road: 0,
+        development: 0,
+      },
     },
     {
       id: "inventory_menu",
       title: "Inventario",
-      buttonIcon: <BackpackIcon sx={{ 
-        fontSize: '4vmin',
-        // transition: (activeMenu!==null && activeMenu.id === 'inventory_menu')
-      }} />,
+      buttonIcon: (
+        <BackpackIcon
+          sx={{
+            fontSize: "4vmin",
+          }}
+        />
+      ),
       color: "warning",
       disabled: false,
-      items: [
-        { icon: <BackpackIcon />, text: "Item 3" },
-        { icon: <BackpackIcon />, text: "Item 4" },
-      ],
+      items: {
+        sheep: 0,
+        wood: 0,
+        wheat: 0,
+        clay: 0,
+        rocks: 0,
+      },
     },
     {
       id: "trade_menu",
       title: "Scambi",
-      buttonIcon: <CurrencyExchangeIcon sx={{ 
-        fontSize: '4vmin',
-        // transition: (activeMenu!==null && activeMenu.id === 'trade_menu')
-      }} />,
+      buttonIcon: (
+        <CurrencyExchangeIcon
+          sx={{
+            fontSize: "4vmin",
+          }}
+        />
+      ),
       color: "error",
       disabled: true,
-      items: [
-        { icon: <CurrencyExchangeIcon />, text: "Item 5" },
-        { icon: <CurrencyExchangeIcon />, text: "Item 6" },
-      ],
     },
   ];
 
   return (
     <>
-      <Paper sx={{
-        visibility: (activeMenu !== null) ? 'visible' : 'hidden',
-        left: '3vw',
-        bottom: '15vh',
-        position:'absolute',
-        zIndex: 10,
-        height: "40vh",
-        width: "15vw",
-        // animation: (activeMenu !== null) ? 'appear 1s' : 'appear 1s reverse'
-      }}
+      <Paper
+        sx={{
+          visibility: activeMenu !== null ? "visible" : "hidden",
+          left: "3vw",
+          bottom: "15vh",
+          position: "absolute",
+          zIndex: 10,
+          height: "fitContent",
+          width: "15vw",
+          // animation: (activeMenu !== null) ? 'appear 1s' : 'appear 1s reverse'
+        }}
       >
         {activeMenu !== null && (
           <>
             {" "}
             <DialogTitle>{activeMenu.title}</DialogTitle>
             <DialogContent>
-              <Menu>{renderMenuItems(activeMenu.items)}</Menu>
+              {activeMenu.id === "inventory_menu"
+                ? renderInventoryItems(activeMenu.items)
+                : renderCraftingItems(activeMenu.items)
+              }
             </DialogContent>
           </>
         )}
@@ -103,9 +150,9 @@ export default function MainMenu() {
         elevation={3}
         sx={{
           width: "15vw",
-          display: 'flex',
+          display: "flex",
           position: "absolute",
-          justifyContent: 'space-around',
+          justifyContent: "space-around",
           zIndex: 10,
           left: "3vw",
           bottom: "3vh",
@@ -114,20 +161,19 @@ export default function MainMenu() {
         {menus.map((menu) => (
           <div key={menu.id}>
             <IconButton
-              sx={{ 
-                borderRadius: 2, 
-                margin: 1, 
-                scale: 1.5 
-                }}
+              sx={{
+                borderRadius: 2,
+                margin: 1,
+                scale: 1.5,
+              }}
               color={menu.color}
               disabled={menu.disabled}
               onClick={(event) => {
-                if (activeMenu === null) 
-                  setActiveMenu(menus.filter(el => el.id === menu.id)[0]);
+                if (activeMenu === null)
+                  setActiveMenu(menus.filter((el) => el.id === menu.id)[0]);
                 else if (activeMenu.id !== menu.id)
-                  setActiveMenu(menus.filter(el => el.id === menu.id)[0]);
-                else
-                  setActiveMenu(null);
+                  setActiveMenu(menus.filter((el) => el.id === menu.id)[0]);
+                else setActiveMenu(null);
               }}
             >
               {menu.buttonIcon}
---
+++ client/src/helpers/GUI_helpers.js
@@ -0,0 +1,51 @@
+import { ReactComponent as SheepIcon } from "../assets/resources_icons/sheep.svg";
+import { ReactComponent as WoodIcon } from "../assets/resources_icons/wood.svg";
+import { ReactComponent as WheatIcon } from "../assets/resources_icons/wheat.svg";
+import { ReactComponent as ClayIcon } from "../assets/resources_icons/clay.svg";
+import { ReactComponent as RocksIcon } from "../assets/resources_icons/rocks.svg";
+import { ReactComponent as TownIcon } from "../assets/resources_icons/town.svg" 
+import { ReactComponent as CityIcon } from "../assets/resources_icons/city.svg" 
+import { ReactComponent as RoadIcon } from "../assets/resources_icons/road.svg" 
+import { ReactComponent as DevelopmentIcon } from "../assets/resources_icons/development.svg" 
+
+export const inventory = [
+  {
+    name: "sheep",
+    icon: SheepIcon,
+  },
+  {
+    name: "wood",
+    icon: WoodIcon,
+  },
+  {
+    name: "wheat",
+    icon: WheatIcon,
+  },
+  {
+    name: "clay",
+    icon: ClayIcon,
+  },
+  {
+    name: "rocks",
+    icon: RocksIcon,
+  },
+];
+
+export const crafting = [
+  {
+    name: "town",
+    icon: TownIcon
+  },
+  {
+    name: "city",
+    icon: CityIcon
+  },
+  {
+    name: "road",
+    icon: RoadIcon
+  },
+  {
+    name: "development",
+    icon: DevelopmentIcon
+  },
+]
